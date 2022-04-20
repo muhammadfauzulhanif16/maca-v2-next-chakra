95243d0b8ae1f890682f48c29cfc0b4a40c3e6de
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2022-04-20 11:29:56 +0700

    Create reading page

--- src/pages/reading/index.tsx
+++ src/pages/reading/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { Shelf } from "../../components/Shelf";
 
-const Reading: FC<{}> = () => {
+const Reading: FC<{}> = (): JSX.Element => {
   return (
     <Shelf
       titlePage="Reading"
