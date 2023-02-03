3fd0cb0da26d49a65f3420cf3c09e521f3e3dbc3
muhammadfauzulhani16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-02-03 14:11:29 +0700

    fix error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { store } from "../app";
-import { Chakra } from "../Chakra";
+// import { Chakra } from "../Chakra";
 import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import NProgress from "nprogress";
 import "../../public/nprogress.css";
+import { theme } from "../theme";
+import {
+  ChakraProvider,
+} from "@chakra-ui/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
@@ -31,9 +35,12 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <Provider store={store}>
-      <Chakra cookies={pageProps.cookies}>
-        <Component {...pageProps} />
-      </Chakra>
+      <ChakraProvider  theme={theme}>
+      {/*<Chakra cookies={pageProps.cookies}>*/}
+      {/*  */}
+      {/*</Chakra>*/}
+      <Component {...pageProps} />
+      </ChakraProvider>
     </Provider>
   );
 };
